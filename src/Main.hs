d0f6d69f8c8c90053b72ff2cd85c737b3dce6d33
Hawk Weisman <hi@hawkweisman.me>
2015-04-15 14:33:50 -0400

    Elaborate on types for main, add Action type

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,17 @@
 module Main where
 ------------------------------------------------------------------------
-import Boid
+
+import Boids
 ------------------------------------------------------------------------
+
+-- A global world state is just a list of boids.
 type World  = [Boid]
+
+-- Update the entire world state by mapping an Action across each Boid
 type Update = World -> World
+
+-- Action update for a Boid. The Update function should map this across the boids
+type Action = World -> (Boid -> Boid) 
 
 main :: IO ()
 main = putStrLn "This program needs to be written :("
