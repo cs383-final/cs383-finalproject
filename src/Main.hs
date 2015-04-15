c0ebcdeef86df79a0431118029b557ce86b01990
Willem Yarbrough <yarbroughw@allegheny.edu>
2015-04-15 15:53:33 -0400

    Main.update now typechecks

--- src/Main.hs
+++ src/Main.hs
@@ -9,11 +9,11 @@ import Boids
 type World  = [Boid]
 
 -- |Action update for a 'Boid'. The 'update' function should map this across the boids
-type Action = World -> (Boid -> Boid) 
+type Action = World -> Boid -> Boid
 
 -- |Update the entire world state by mapping an 'Action' across each 'Boid'
 update :: Action -> World -> World
-update a w = map a w
+update a w = map (a w) w
 
 main :: IO ()
 main = putStrLn "This program needs to be written :("
