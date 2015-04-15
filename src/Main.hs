4c6f8849c11aa0203e9d5aaaccdf706e83ebe766
Hawk Weisman <hi@hawkweisman.me>
2015-04-15 14:29:44 -0400

    Main update world type

--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,9 @@
+module Main where
+------------------------------------------------------------------------
+import Boid
+------------------------------------------------------------------------
+type World  = [Boid]
+type Update = World -> World
+
 main :: IO ()
 main = putStrLn "This program needs to be written :("
