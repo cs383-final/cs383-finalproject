3ed47565059b45d1cd9c64d2461671328f37dc36
Hawk Weisman <hi@hawkweisman.me>
2015-04-15 14:51:22 -0400

    Concrete implementation of action

--- src/Main.hs
+++ src/Main.hs
@@ -7,11 +7,12 @@ import Boids
 -- A global world state is just a list of boids.
 type World  = [Boid]
 
--- Update the entire world state by mapping an Action across each Boid
-type Update = World -> World
-
 -- Action update for a Boid. The Update function should map this across the boids
 type Action = World -> (Boid -> Boid) 
+
+-- Update the entire world state by mapping an Action across each Boid
+update :: Action -> World -> World
+update a w = map a w
 
 main :: IO ()
 main = putStrLn "This program needs to be written :("
