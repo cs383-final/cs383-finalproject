e44e4e1b4bb91c4bdef8daa50b7627fcace61089
Hawk Weisman <hawk@meteorcodelabs.com>
2015-04-21 18:46:20 -0400

    Try pointful syntax
    Maybe the type checker will understand it now?

--- src/Boids.hs
+++ src/Boids.hs
@@ -34,7 +34,7 @@ positions = map position
 centre :: [Boid] -> Vector
 centre boids =
     let m = length boids in
-    sumV . map(^/ m) . boids positions
+    sumV (map (^/ m) (boids positions))
 
 -- |Find the separation steer vector for a boid given a neighborhood.
 -- |
@@ -44,7 +44,7 @@ centre boids =
 separation :: Boid -> [Boid] -> Vector
 separation self neighbors =
     let p = self position in
-    sumV . map(^-^ p) . neighbors positions
+    sumV (map (^-^ p) (neighbors positions))
 
 -- |Find the cohesion steer vector for a boid given a neighborhood.
 -- |
