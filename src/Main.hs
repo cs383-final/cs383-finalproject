d12254ac5852294732ff04421861c40ad7ec0c35
Hawk Weisman <hawk@meteorcodelabs.com>
2015-04-27 21:08:47 -0400

    Start on option parsing

--- src/Main.hs
+++ src/Main.hs
@@ -8,9 +8,43 @@ import Linear.V2
 import Graphics.Gloss hiding (Point)
 import Graphics.Gloss.Interface.Pure.Simulate hiding (Point)
 import Control.Monad.Random
+import System.Environment( getArgs )
+import System.Console.GetOpt
 
 ------------------------------------------------------------------------
 
+data Options = Options
+  { optDebug       :: Bool
+  , optMode        :: Action
+  }
+
+defaultOptions  = Options
+  { optDebug    = False
+  , optMode     = eqWeightStep
+  }
+
+options :: [OptDescr (Options -> Options)]
+options =
+  [ Option ['d']     ["debug"]
+      (NoArg (\ opts -> opts { optDebug = True}))
+      "show debug mode"
+  , Option ['c'] ["cohesive"]
+      (NoArg (\ opts -> opts { optMode = cohesiveStep }))
+      "cohesive behaviour"
+  , Option ['s'] ["swarm"]
+      (NoArg (\ opts -> opts { optMode = swarmStep }))
+      "swarming behaviour"
+  , Option ['e'] ["equal"]
+      (NoArg (\ opts -> opts { optMode = eqWeightStep}))
+      "equal weighted behavioir"
+  ]
+
+argOpts :: [String] -> IO (Options, [String])
+argOpts argv =
+  case getOpt Permute options argv of
+     (o,n,[]  ) -> return (foldl (flip id) defaultOptions o, n)
+     (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
+ where header = "Usage: -dcsv"
 
 drawBoid :: Boid -> Picture
 drawBoid (Boid (V2 xpos ypos) (V2 xvel yvel) rad) =
@@ -35,12 +69,15 @@ boundsCheck (width, height) = map modBoid
         inHeight = inBounds $ fromIntegral height
 
 advanceWorld :: (Int, Int) -> ViewPort -> Float -> World -> World
-advanceWorld dims _ _ = boundsCheck dims . update swarmStep
+advanceWorld dims _ _ = boundsCheck dims . update cohesiveStep
 
 main :: IO ()
 main = do
 
   let dims = (1000, 800)
+
+  args <- getArgs
+  let ( flags, nonOpts, msgs ) = getOpt RequireOrder options args
 
   pos_x <- evalRandIO (initPos 20)
   pos_y <- evalRandIO (initPos 20)
